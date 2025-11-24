5375653e6368d1117237cdef99ebb2db9ac6ed3a
gap816-png <gap816@gmail.com>
2025-11-24 18:35:46 +0000

    Aqsa fixed Code
    Added Main menu Scene, fixed UIManager code. etc

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,6 +5,7 @@ using TMPro;
 public class UIManager : MonoBehaviour
 {
 
+    private int currentBuildIndex;
     public static UIManager instance = null; 
     
     [SerializeField]
@@ -28,6 +29,8 @@ public class UIManager : MonoBehaviour
     void Update()
     {
         myTMPText.text = SceneManager.GetActiveScene().name;
+
+        currentBuildIndex = SceneManager.GetActiveScene().buildIndex; //resets current build index variable to the active scene index
     }
 
     public void OnNextSceneBtClick()
@@ -37,23 +40,41 @@ public class UIManager : MonoBehaviour
               GameManager.instance.sceneIndex = 1;
         }
 
-        SceneManager.LoadScene(GameManager.instance.sceneIndex);
-        GameManager.instance.sceneIndex +=1;
+
+        //if current build index variable is less than 1, then we can move up scenes, make sure to change this number from 1 to the total number of [scenes -1.]
+        if(currentBuildIndex < 1) //once we add more scenes, this number becomes total number of scenes -1 *********
+        {
+            //loading next scene
+            SceneManager.LoadScene(GameManager.instance.sceneIndex);
+            GameManager.instance.sceneIndex +=1;
+
+            //to check if we're moving up scenes
+            Debug.Log("We moving up scenes");
+        }
        
        //can add a quit here and a return*******************FOR FINAL BUILD ADD THIS
         
     }
 
-    public void OnBackSceneBtClick()
+
+    public void OnBackSceneBtClick() //button for back
     {
         if(GameManager.instance.sceneIndex >= SceneManager.sceneCountInBuildSettings)
         {
               GameManager.instance.sceneIndex = 1;
         }
-        //3 scenes so far
-        SceneManager.LoadScene(GameManager.instance.sceneIndex);
-        GameManager.instance.sceneIndex -=1;
-       
+         //when we add in main menu scene with skeleton hand, that will be the new index 0, so we will change number to 1. ************
+         if(currentBuildIndex > 0) // if current build index is greater than 0 (not in scene 1), we can go back to previous scene
+        {   
+            //loading previous scene
+            SceneManager.LoadScene(GameManager.instance.sceneIndex);
+            GameManager.instance.sceneIndex -=1;
+
+            //to check if we're moving down scenes
+            Debug.Log("We moving down scenes :()");
+        }
+            
+       //kerri said we can just delete the back button from the 1st scene OR can add an if statement instance as 
        //can add a quit here and a return*******************FOR FINAL BUILD ADD THIS
         
     }
