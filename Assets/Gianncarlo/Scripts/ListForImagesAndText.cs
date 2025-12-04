7b4a6e0ce13aa2d47aea8e3c143881e2efba3251
gap816-png <gap816@gmail.com>
2025-12-04 18:21:43 +0000

    Matt made basic script for lists and retrieving them via specific scene
    made 2 scripts for this (ManageImageList) (ListforImagesandList)

--- Assets/Gianncarlo/Scripts/ListForImagesAndText.cs
+++ Assets/Gianncarlo/Scripts/ListForImagesAndText.cs
@@ -1,34 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ListForImagesAndText : MonoBehaviour
 {
-    /*[SerializeField]
+    [SerializeField]
     private List<GameObject> parentImageList = new List<GameObject>();
 
-    [SerializeField]
-    private List<GameObject> imageList = new List<GameObject>();
 
     private int currentBuildIndex = 0;
-    public int images;
+    public int SceneIndex = 1;
 
-    void Update() 
+
+    void Start()
     {
     
-    if (Input.GetKeyDown(KeyCode.Space))
-       {
-            for (parentImageList = 0; parentImageList < transform.childCount; parentImageList++)
+
+
+    }
+    void Update() 
+    {
+        
+        
+    }
+
+    public void OnSceneChangeBt()
+    {
+        for (int i = 0; i < parentImageList.Count; i++)
+        {
+            for (int j = 0; j < parentImageList[i].transform.childCount; j++ )
             {
-                transform.GetChild(parentImageList).gameObject.SetActive(false);
-
-                if (imageList.Contains(transform.GetChild(parentImageList).gameObject))
-                {
-                    transform.GetChild(parentImageList).gameObject.SetActive(true);
-                }
+                parentImageList[i].transform.GetChild(j).gameObject.SetActive(false);
             }
-    
-        }   
-    }*/
-}
+        }
 
+        SceneIndex = SceneManager.GetActiveScene().buildIndex;
+        parentImageList[SceneIndex].GetComponent<ManageImageList>().imageList[0].SetActive(true);
+
+    }
+    //need another button to turn em off?
+
+}
---
+++ Assets/Gianncarlo/Scripts/ManageImageList.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ManageImageList : MonoBehaviour
+{
+
+    public List<GameObject> imageList = new List<GameObject>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
